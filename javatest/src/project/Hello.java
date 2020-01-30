4775c82201c59982c0851114456f7569e13560a7
JmKanmo <nebi25@naver.com>
2020-01-30 14:50:38 +0900

    go

--- javatest/src/project/Hello.java
+++ javatest/src/project/Hello.java
@@ -3,8 +3,6 @@ package project;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int priority = Integer.parseInt("1순위".substring(0, 1));
-		System.out.println(priority);
+		System.out.println("hello man");
 	}
 }
