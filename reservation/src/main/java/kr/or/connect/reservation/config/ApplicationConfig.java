21a69a1ea23938d91f13ff9adbe5ba1d1b732d5d
JmKanmo <nebi25@naver.com>
2020-02-25 23:04:11 +0900

    코드리뷰반영(C-BE)

--- reservation/src/main/java/kr/or/connect/reservation/config/ApplicationConfig.java
+++ reservation/src/main/java/kr/or/connect/reservation/config/ApplicationConfig.java
@@ -1,21 +1,14 @@
 package kr.or.connect.reservation.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan(basePackages = { "kr.or.connect.reservation.dao", "kr.or.connect.reservation.service" })
 @Import({ DBConfig.class })
 @PropertySource(value = { "classpath:application.properties" })
 public class ApplicationConfig {
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-		pspc.setIgnoreUnresolvablePlaceholders(true);
-		return pspc;
-	}
+	
 }
--- reservation/src/main/java/kr/or/connect/reservation/config/DBConfig.java
+++ reservation/src/main/java/kr/or/connect/reservation/config/DBConfig.java
@@ -3,9 +3,9 @@ package kr.or.connect.reservation.config;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,26 +14,19 @@ import org.springframework.transaction.annotation.TransactionManagementConfigure
 @Configuration
 @EnableTransactionManagement
 public class DBConfig implements TransactionManagementConfigurer {
+	private Environment env;
 
-	@Value("${spring.datasource.driverClassName}")
-	private String driverClassName;
-
-	@Value("${spring.datasource.url}")
-	private String url;
-
-	@Value("${spring.datasource.password}")
-	private String password;
-
-	@Value("${spring.datasource.user}")
-	private String user;
+	public DBConfig(Environment env) {
+		this.env = env;
+	}
 
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName(driverClassName.trim());
-		dataSource.setUrl(url.trim());
-		dataSource.setUsername(user.trim());
-		dataSource.setPassword(password.trim());
+		dataSource.setDriverClassName(byEnvironment("datasource.mysql.driver"));
+		dataSource.setUrl(byEnvironment("datasource.mysql.url"));
+		dataSource.setUsername(byEnvironment("datasource.mysql.user"));
+		dataSource.setPassword(byEnvironment("datasource.mysql.password"));
 		return dataSource;
 	}
 
@@ -45,5 +38,10 @@ public class DBConfig implements TransactionManagementConfigurer {
 	@Bean
 	public PlatformTransactionManager transactionManger() {
 		return new DataSourceTransactionManager(dataSource());
+	}
+
+	private String byEnvironment(String before) {
+		String key = before + "." + env.getProperty("environment");
+		return env.getProperty(key);
 	}
 }
